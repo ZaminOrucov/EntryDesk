ba14081ced664455e5f676e4f7bb8499a77ac96e
ZaminOrucov <zamin.h.orujov@gmail.com>
2025-11-17 10:30:41 +0400

    Initial Commit

---
+++ EntryDeskTests/EntryDeskTests.swift
@@ -0,0 +1,17 @@
+//
+//  EntryDeskTests.swift
+//  EntryDeskTests
+//
+//  Created by Zamin Orucov on 11/17/25.
+//
+
+import Testing
+@testable import EntryDesk
+
+struct EntryDeskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
